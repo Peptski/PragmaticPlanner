334f30a5758aafd681169e28d0cd6410d290159e
Lukas Andersson <lukas.s.andersson@gmail.com>
2022-08-12 15:51:00 +0200

    autofill date

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { CommonModule, formatDate } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css'],
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') dateForm!: NgForm;
+
   constructor() {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      let date = new Date();
+      this.dateForm.setValue({
+        date: date.toLocaleDateString(),
+        time: date.toLocaleTimeString().slice(0, -3),
+      });
+    });
+  }
 
   ngOnInit(): void {}
 }
