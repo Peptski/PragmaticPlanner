dcf95e3f91be412ad31df25dbd931b9496587234
Lukas Andersson <lukas.s.andersson@gmail.com>
2022-08-16 09:47:41 +0200

    ngrx actions

---
+++ src/app/search-page/data-access/actions/search-page-api.actions.ts
@@ -0,0 +1,6 @@
+import { createAction } from "@ngrx/store"
+
+export const apiPatternSuccess = createAction('[Search Page] API pattern success')
+export const apiPatternFail = createAction('[Search Page] API pattern fail')
+export const apiTripSuccess = createAction('[Search Page] API trip success')
+export const apiTripFail = createAction('[Search Page] API trip fail')
---
+++ src/app/search-page/data-access/actions/search-page.actions.ts
@@ -0,0 +1,8 @@
+import { createAction, props } from "@ngrx/store"
+
+export const enterSubmit = createAction('[Search Page] Enter submit')
+export const buttonSubmit = createAction('[Search Page] Button submit')
+export const updatedSearchParams = createAction('[Search Page] Update search params', props<{entry : string}>())
+export const updateTimeMode = createAction('[Search Page] Update time mode', props<{ mode : string}>())
+export const updateTime = createAction('[Search Page] Update time', props<{time : string}>())
+export const updateDate = createAction('[Search Page] Update date', props<{date : string}>())
