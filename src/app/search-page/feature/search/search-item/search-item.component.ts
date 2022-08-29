c421ccac050ea7b77b60c4c9e7ef8fc3683682dc
Lukas Andersson <lukas.s.andersson@gmail.com>
2022-08-29 10:23:53 +0200

    search refactor ngrx

--- src/app/search-page/feature/search/search-item/search-item.component.ts
+++ src/app/search-page/feature/search/search-item/search-item.component.ts
@@ -1,15 +1,11 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
-import { Stop } from 'src/app/search-page/utils/stop.model';
-import { SearchService } from 'src/app/search-page/data-access/search.service';
 import { Store } from '@ngrx/store';
 import {
   enterSubmit,
   updateSearchParams,
 } from 'src/app/search-page/data-access/actions/search-page.actions';
-import { selectSearchParams } from 'src/app/search-page/data-access/reducers/search.reducer';
 
 @Component({
   selector: 'app-search-item',
@@ -18,43 +14,19 @@ import { selectSearchParams } from 'src/app/search-page/data-access/reducers/sea
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.css'],
 })
-export class SearchItemComponent implements OnInit, OnDestroy {
-  @Input() targetStop = '';
+export class SearchItemComponent implements OnInit {
+  @Input() targetStop!: string;
   @Input() index!: number;
   templateElement = false;
   searchForm!: FormGroup;
-  patternMatching: Stop[] = [];
+  // patternMatching: Stop[] = [];
 
-  subscriptions: Subscription[] = [];
-
-  constructor(private searchService: SearchService, private store: Store) {}
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
     this.searchForm = new FormGroup({
       search: new FormControl(this.targetStop),
     });
-
-    // this.subscriptions.push(
-    //   this.searchForm.valueChanges.subscribe(({ search }) => {
-    //     this.searchService.patternMatching(search, this.index);
-    //     this.store.dispatch(
-    //       updateSearchParams({
-    //         search: search,
-    //         index: this.index,
-    //       })
-    //     );
-    //   })
-    // );
-
-    // this.subscriptions.push(
-    //   this.searchService.searchUpdated.subscribe((patternMatching) => {
-    //     this.patternMatching = patternMatching;
-    //   })
-    // );
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 
   toggleActive() {
